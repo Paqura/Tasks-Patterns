cfdadd7e57d3b9284fe4ab4fcfca0fdaddb34ca3
SlavalS <slavaavals616@gmail.com>
2018-07-06 10:27:46 +0400

    pubsub

---
+++ patterns/PubSub.js
@@ -0,0 +1,45 @@
+const EventBus = {
+  channels: {},
+  subscribe (channelName, listener) {
+    if (!this.channels[channelName]) {
+      this.channels[channelName] = []
+    }
+    this.channels[channelName].push(listener)
+  },
+
+  publish (channelName, data) {
+    const channel = this.channels[channelName]
+    if (!channel || !channel.length) {
+      return
+    }
+
+    channel.forEach(listener => listener(data))
+  }
+}
+
+class Order {
+  constructor (params) {
+    this.params = params
+  }
+
+  save () {
+    console.log('Order saved')
+    EventBus.publish('order/new', {
+      userEmail: this.params.userEmail
+    })
+  }
+}
+
+class Mailer {
+  constructor () {
+    EventBus.subscribe('order/new', this.sendPurchaseEmail)
+  }
+
+  sendPurchaseEmail (params) {
+    console.log(`Email send to ${params.userEmail}`)
+  }
+}
+
+const mailer = new Mailer()
+const order = new Order({userEmail: 'slavaavals616@gmail.com'})
+order.save()
