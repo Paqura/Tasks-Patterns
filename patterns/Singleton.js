919964e5c72f2bced6cd4fb7342b7c2af8b24c47
SlavalS <slavaavals616@gmail.com>
2018-07-06 09:43:39 +0400

    singleton

--- patterns/Singleton.js
+++ patterns/Singleton.js
@@ -3,17 +3,22 @@ class Singleton {
 
   constructor() {
     if(Singleton.instance) {
-      return this;
+      return Singleton.instance;
     }
+    Singleton.instance = this;
     this.counter = 0;    
   }
 
   getCounter() {
     return this.counter;
   }
-
+  
   upCounter() {
     this.counter++;
   }
 }
 
+const s = new Singleton();
+const s2 = new Singleton();
+
+console.log(s === s2);
