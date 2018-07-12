9e7d77ca8b5d6f636c1732bede16ab89a7c37c55
SlavalS <slavaavals616@gmail.com>
2018-07-12 09:48:48 +0400

    вторая версия singletone

--- patterns/Singleton.js
+++ patterns/Singleton.js
@@ -22,3 +22,32 @@ const s = new Singleton();
 const s2 = new Singleton();
 
 console.log(s === s2);
+
+/**
+ * Так же Singletone можно описать, как
+ */
+
+class Single {
+  constructor() {
+    if(!Single.instance) {
+      this.data = [];
+      Single.instance = this;
+    }
+    return Single.instance;
+  }
+  
+  addItem(item) {
+    this.data.push(item);
+  }
+  
+  getData() {
+    console.log(this.data);
+  }
+}
+
+const s1 = new Single();
+const s2 = new Single();
+
+s2.addItem('hello');
+
+console.log(s1, s2);
