47a6a40d368cac63c070ae172804fc7af86ea60f
Anastasiia <21364097+Koshishe@users.noreply.github.com>
2023-05-16 18:05:12 +0700

    PTS-42: Исправить верстку в карточке-секции

--- frontend/src/components/HomePage/components/Advantages/components/WelcomeToContact/index.tsx
+++ frontend/src/components/HomePage/components/Advantages/components/WelcomeToContact/index.tsx
@@ -7,8 +7,8 @@ import { sanitizeText } from '@/utils/sanitaze'
 
 import styles from './index.module.scss'
 
-const title = 'Ready to&nbsp;secure your business?&nbsp;'
-const accentText = 'Get in&nbsp;touch'
+const title = 'Ready to&nbsp;secure your business?'
+const accentText = 'Get&nbsp;in&nbsp;touch'
 const descriptionText =
     'Contact&nbsp;us today to&nbsp;learn how our cybersecurity solutions can help protect your organization from threats and vulnerabilities. Don&rsquo;t wait until it&rsquo;s too late&nbsp;&mdash; take action now to&nbsp;ensure the safety and integrity of&nbsp;your data.'
 
@@ -16,7 +16,7 @@ export const WelcomeToContact: React.FC = () => {
     return (
         <div className={styles.wrapper}>
             <Heading level={2} className={styles.title}>
-                {title}
+                {title}{' '}
                 <span
                     className={styles.accentText}
                     dangerouslySetInnerHTML={{ __html: sanitizeText(accentText) }}
