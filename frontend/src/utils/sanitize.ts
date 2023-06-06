6e9257bb0aa1d878acfa45504315d88a8189e289
Pavel Yagodin <pavel.yagodin@gmail.com>
2023-06-06 18:48:51 +0300

    PTS-139: Исправить открытие ссылки в той же вкладке

--- frontend/src/utils/sanitize.ts
+++ frontend/src/utils/sanitize.ts
@@ -1,3 +1,23 @@
 import DOMPurify from 'isomorphic-dompurify'
 
+const TEMPORARY_A_TARGET = 'data-sanitize-target'
+
+DOMPurify.addHook('beforeSanitizeAttributes', function (node) {
+    if (node.tagName === 'A') {
+        const target = node.getAttribute('target')
+        if (target) {
+            node.setAttribute(TEMPORARY_A_TARGET, target)
+        }
+    }
+})
+
+DOMPurify.addHook('afterSanitizeAttributes', function (node) {
+    if (node.tagName === 'A') {
+        const target = node.getAttribute(TEMPORARY_A_TARGET)
+        if (target) {
+            node.setAttribute('target', target)
+        }
+    }
+})
+
 export const sanitizeText = (text: string) => DOMPurify.sanitize(text)
