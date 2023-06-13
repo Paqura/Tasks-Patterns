6bbf04ce814c789a0de35336dfa1f94f6faf6e2e
Maksim Kononov <m.kononov@ptsecurity.com>
2023-06-13 20:00:32 +0200

    PTS-191: Исправить логику поиска для значений в начале абзаца

--- admin/src/utils/dataTransformations.ts
+++ admin/src/utils/dataTransformations.ts
@@ -1,5 +1,21 @@
 import DOMPurify from 'isomorphic-dompurify';
 
+const TEXT_NODE_TYPE = 3;
+
+/*
+  Тут мы проходим по всем текст-нодам и добавляем пробел в конце,
+  таким бразом мы отделяем знаки пунктуации от последующего текста.
+  Основная задача - решить проблему с точкой, которая при соединении слов портит поиск: https://github.com/meilisearch/meilisearch/issues/2962
+*/
+
+DOMPurify.addHook('afterSanitizeElements', function (node) {
+  // @ts-expect-error
+  if(node.nodeType === TEXT_NODE_TYPE){
+    // @ts-expect-error
+    node.textContent += ' ';
+  }
+});
+
 const sanitizeText = (text: string) => DOMPurify.sanitize(text, {ALLOWED_TAGS: []});
 
 type TObject = Record<string, unknown>;
@@ -37,3 +53,4 @@ export const transformData = (obj: TObject, propsToRemove: string[]) => {
 
   return obj;
 };
+
