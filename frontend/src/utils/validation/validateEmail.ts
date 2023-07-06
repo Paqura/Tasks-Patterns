6860722ef56a7e5fb29fa203c77fd8ad5867b4bd
Maksim Kononov <m.kononov@ptsecurity.com>
2023-07-06 09:20:04 +0200

    PTS-238: Доработал функцию валидации email, чтобы она пропускала домен global

--- frontend/src/utils/validation/validateEmail.ts
+++ frontend/src/utils/validation/validateEmail.ts
@@ -3,5 +3,5 @@ import { ValidationValueMessage } from 'react-hook-form'
 export const validateEmail: ValidationValueMessage<RegExp> = {
     message: 'name@example.com',
     // eslint-disable-next-line no-useless-escape
-    value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+    value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,6}$/g,
 }
